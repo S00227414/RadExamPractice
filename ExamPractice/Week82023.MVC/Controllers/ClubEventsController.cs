2e795d6767938364f2f41559aed45bc75e6c4d95
S00227414 <s00227414@atu.ie>
2023-12-31 17:27:22 +0000

    Dropdown Box implemented

--- ExamPractice/Week82023.MVC/Controllers/ClubEventsController.cs
+++ ExamPractice/Week82023.MVC/Controllers/ClubEventsController.cs
@@ -1,5 +1,6 @@
 ﻿using ClubModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Tracker.WebAPIClient;
 
@@ -8,9 +9,42 @@ namespace Week82023.MVC.Controllers
     public class ClubEventsController : Controller
     {
         ClubsContext db = new ClubsContext();
+
+        private List<SelectListItem> FillClubs()
+        {
+            List<SelectListItem> items = new List<SelectListItem>();
+            {
+                var clubs = db.Clubs.ToList();
+                foreach (var item in clubs)
+                    items.Add(new SelectListItem()
+                    {
+                        Value = item.ClubId.ToString(),
+                        Text = item.ClubName
+                    });
+            }
+            return items;
+        }
         public IActionResult Index()
         {
-            return View();
+            // Activity Tracker
+            ActivityAPIClient.Track(StudentID: "S00227414", StudentName: "Levi Gilmartin",
+                activityName: "RAD301 Week 8 Lab 2023", Task: "Implementing All Club Detail Filter");
+
+            List<SelectListItem> items = FillClubs();
+            items.First().Selected = true;
+            ViewBag.Clubs = items;
+            int cid = Int32.Parse(items.First().Value);
+            return View(db.Clubs.FirstOrDefault(c => c.ClubId == cid));
+        }
+        [HttpPost]
+        public IActionResult Index(Club model)
+        {
+            
+
+            List<SelectListItem> items = FillClubs();
+            items.First(s => s.Value == model.ClubId.ToString());
+            ViewBag.Clubs = items;
+            return View(db.Clubs.FirstOrDefault(c => c.ClubId == model.ClubId));
         }
 
         public async Task<ActionResult> AllClubDetails(string ClubName = null)
